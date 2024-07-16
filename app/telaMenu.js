27342391f01cae40211a2c6e2bc515d099ba909e
FelipeGuttemberg <fgos1@aluno.ifal.edu.br>
2024-07-16 16:21:53 -0300

    Iniciando a tela de menu

---
+++ app/telaMenu.js
@@ -0,0 +1,28 @@
+import * as React from 'react'; 
+import { TextInput } from 'react-native-paper';
+import { StyleSheet, Text, View, Image, ImageBackground, TouchableOpacity } from 'react-native';
+import { Link } from 'expo-router';
+
+const telaMenu = () => {
+return(
+
+   <></>
+    
+
+
+
+
+
+
+
+
+
+
+
+ );
+}
+
+
+
+
+export default telaMenu
